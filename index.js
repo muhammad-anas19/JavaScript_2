cafe2a7aa20e37f7866f71a124a8a1eb824f1231
Muhammad Anas <157979121+muhammad-anas19@users.noreply.github.com>
2024-09-11 11:35:33 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+// var age = 25;
+
+// if(true) {
+//     console.log(age);
+// }
+
+// function solve() {
+//     var age = 25;
+    
+// }
+// console.log(age);
+// solve();
+
+
+// {
+// let a = 10;
+// }
+// console.log(a);
+
+let a = 10;
+a = "Anas";
+a = true;
+console.log(a);
